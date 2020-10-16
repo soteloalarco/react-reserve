c6d2cf721df69cdc20cf030d9836c651ce6d82ae
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-16 15:10:12 -0500

    fetch props from server

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import axios from 'axios'
 
-function Home() {
+function Home(products) {
+  console.log(products)
 
-  React.useEffect(()=>{
-    getProducts()
-  },[])
+  // React.useEffect(()=>{
+  //   getProducts()
+  // },[])
 
-  async function getProducts(){
-      const url='http://localhost:3000/api/products';
-      const response = await axios.get(url);
-      console.log(response.data)
-  }
+  // async function getProducts(){
+  //     const url='http://localhost:3000/api/products';
+  //     const response = await axios.get(url);
+  //     console.log(response.data)
+  // }
 
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const url='http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  // return response data as an object
+  return {products: response.data};
+  // note; this object will b merged with existing props
 }
 
 export default Home;
