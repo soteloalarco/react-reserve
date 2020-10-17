bd01800697b125464529c72e196f16cf6f454736
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-16 19:59:24 -0500

    products stackable and centered in 3 itemsperrow

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -6,6 +6,7 @@ function ProductList({products}) {
     return products.map(product => ({
       header: product.name,
       image: product.mediaUrl,
+      meta: `$${product.price}`,
       color: 'teal',
       fluid: true,
       childKey: product._id,
@@ -13,7 +14,7 @@ function ProductList({products}) {
     }))
   }
   
-  return <Card.Group items={mapProductsToItems(products)} />
+  return <Card.Group stackable itemsPerRow="3" centered items={mapProductsToItems(products)} />
 }
 
 export default ProductList;
