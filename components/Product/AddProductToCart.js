30214b3ee14cbb9c89bc5747bba3039cb78cc45f
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-21 20:00:33 -0500

    product pages added and base url setup

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,16 @@
+import {Icon, Input, Placeholder} from 'semantic-ui-react'
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return <Input 
+    type="number"
+    min= "1"
+    Placeholder="Quantity"
+    action={{ 
+      color: 'orange',
+      content: 'Add to cart',
+      icon: "plus cart"
+    }}
+    />
 }
 
 export default AddProductToCart;
